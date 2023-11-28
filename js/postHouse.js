42d4a63f6ef13aa847ea220c4f00db4e293f9f44
anorfire <anorfire@gmail.com>
2023-11-28 14:26:06 +0800

    上下頁切換、首頁、狀態條

--- js/postHouse.js
+++ js/postHouse.js
@@ -10,16 +10,23 @@ const prevBtn = $("#prev");
 const nextBtn = $("#next");
 const progress = $("#progress");
 const nodeElements = $$(".node");
+const sections = $$('.section');
 const min = 0, max = nodeElements.length - 1;
-let currentActive = 0;
+let currentSectionIndex = 0; // section追蹤
+let currentActive = 0; // 頁數追蹤
 
+//頁面控制
 nextBtn.addEventListener("click", () => {
     if (nextBtn.classList.contains('disabled')) return;
-    if (currentActive >= max - 1) {
+    if (currentActive >= max -1) {
         // currentActive = min;
         handleClass(nextBtn).addClass("disabled").removeClass("active");
     }
     if (currentActive <= max - 1) {
+        sections[currentSectionIndex].classList.remove('active')
+        // 找到下一個section
+        currentSectionIndex++;
+        sections[currentSectionIndex].classList.add('active')
         currentActive++;
     }
     if (currentActive > 0) {
@@ -35,6 +42,9 @@ prevBtn.addEventListener("click", () => {
         handleClass(prevBtn).addClass("disabled").removeClass("active");
     }
     if (currentActive > 0) {
+        sections[currentSectionIndex].classList.remove('active');
+        currentSectionIndex--;
+        sections[currentSectionIndex].classList.add('active');
         currentActive--;
     }
     if (currentActive <= max - 1) {
@@ -43,6 +53,7 @@ prevBtn.addEventListener("click", () => {
     update();
 });
 
+//兩種classList寫法
 function handleClass(el) {
     let methods = {
         addClass,
@@ -62,7 +73,7 @@ function handleClass(el) {
 function update() {
     nodeElements.forEach((item, index) => {
         if (index <= currentActive) {
-            // classLIst属性返回元素的类名，作为DOMTokenList对象
+            // classLIst屬性返回元素的類名，作為DOMTokenList對象
             item.classList.add('active');
         } else {
             item.classList.remove('active');
@@ -71,3 +82,24 @@ function update() {
     //toFixed() 四捨五入到指定小數位
     progress.style.width = (100 / max * currentActive).toFixed(2) + '%';
 }
+
+
+//btn選取
+
+document.addEventListener("DOMContentLoaded", function() {
+    // 拿到所有按鈕
+    var buttons = document.querySelectorAll('.btn-row button');
+
+    // 加點擊事件到所有按鈕
+    buttons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            // 把所有按鈕active屬性刪掉
+            buttons.forEach(function(btn) {
+                btn.classList.remove('active');
+            });
+
+            // 加active到選擇的按鈕
+            button.classList.add('active');
+        });
+    });
+});
