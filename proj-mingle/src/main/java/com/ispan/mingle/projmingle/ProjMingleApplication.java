2995580f513c449b5550f4e6b141e4a154fb186b
gitInterface <97436826+gitInterface@users.noreply.github.com>
2024-01-06 17:31:40 +0800

    create maven project. modify pom.xml adding dependencies. add controller, dao, domain, repository, service, util folder.

---
+++ proj-mingle/src/main/java/com/ispan/mingle/projmingle/ProjMingleApplication.java
@@ -0,0 +1,13 @@
+package com.ispan.mingle.projmingle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjMingleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjMingleApplication.class, args);
+	}
+
+}
