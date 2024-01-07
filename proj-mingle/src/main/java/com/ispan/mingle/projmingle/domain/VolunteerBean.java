1b6a6b5aac15909f9f381e7792afaa1e6e94455b
aura70001 <b0232036@ems.niu.edu.tw>
2024-01-07 13:12:54 +0800

    declare naming strategy

--- proj-mingle/src/main/java/com/ispan/mingle/projmingle/domain/VolunteerBean.java
+++ proj-mingle/src/main/java/com/ispan/mingle/projmingle/domain/VolunteerBean.java
@@ -6,14 +6,14 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-//@Data
-//@Entity
-//@Table(name = "Volunteer")
+ @Data
+ @Entity
+ @Table(name = "Volunteer")
 public class VolunteerBean {
-//    @Id
-//    @Column(name = "userId", columnDefinition = "varchar", length = 100)
+     @Id
+     @Column(name = "userId", columnDefinition = "varchar", length = 100)
     private String userId;
 
-//    @Column(name = "password", columnDefinition = "varchar", length = 20)
+     @Column(name = "password", columnDefinition = "varchar", length = 20)
     private String password;
 }
--- proj-mingle/src/main/java/com/ispan/mingle/projmingle/domain/VolunteerDetailBean.java
+++ proj-mingle/src/main/java/com/ispan/mingle/projmingle/domain/VolunteerDetailBean.java
@@ -6,60 +6,60 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
 
-//@Data
-//@Entity
-//@Table(name = "Volunteer_Detail")
+@Data
+@Entity
+@Table(name = "Volunteer_Detail")
 public class VolunteerDetailBean {
-//	@Id
-//	@Column(name = "fk_userID", columnDefinition = "varchar", length = 100)
+	@Id
+	@Column(name = "fk_userID", columnDefinition = "varchar", length = 100)
 	private String userId;
 
-//	@Column(name = "name", columnDefinition = "nvarchar", length = 20)
+	@Column(name = "name", columnDefinition = "nvarchar", length = 20)
 	private String name;
 
-//	@Column(name = "gender", columnDefinition = "varchar", length = 10)
+	@Column(name = "gender", columnDefinition = "varchar", length = 10)
 	private String gender;
 
-//	@Column(name = "phone", columnDefinition = "varchar", length = 20)
+	@Column(name = "phone", columnDefinition = "varchar", length = 20)
 	private String phone;
 
-//	@Column(name = "email", columnDefinition = "varchar", length = 50)
+	@Column(name = "email", columnDefinition = "varchar", length = 50)
 	private String email;
 
-//	@Column(name = "introduction", columnDefinition = "nvarchar", length = 300)
+	@Column(name = "introduction", columnDefinition = "nvarchar", length = 300)
 	private String introduction;
 
-//	@Column(name = "image", columnDefinition = "varbinary", length = 3000)
-	private Byte[] image;
+	@Column(name = "image", columnDefinition = "varbinary", length = 3000)
+	private byte[] image;
 
-//	@Column(name = "photoSize", columnDefinition = "int")
+	@Column(name = "photoSize", columnDefinition = "int")
 	private Integer photoSize;
 
-//	@Column(name = "photoType", columnDefinition = "varchar", length = 20)
+	@Column(name = "photoType", columnDefinition = "varchar", length = 20)
 	private String photoType;
 
-//	@Column(name = "birth", columnDefinition = "datetime")
+	@Column(name = "birth", columnDefinition = "datetime")
 	private String birth;
 
-//	@Column(name = "fk_country", columnDefinition = "nvarchar", length = 15)
+	@Column(name = "fk_country", columnDefinition = "nvarchar", length = 15)
 	private String country;
 
-//	@Column(name = "background", columnDefinition = "nvarchar", length = 500)
+	@Column(name = "background", columnDefinition = "nvarchar", length = 500)
 	private String background;
 
-//	@Column(name = "language", columnDefinition = "nvarchar", length = 30)
+	@Column(name = "language", columnDefinition = "nvarchar", length = 30)
 	private String language;
 
-//	@Column(name = "hobby", columnDefinition = "nvarchar", length = 100)
+	@Column(name = "hobby", columnDefinition = "nvarchar", length = 100)
 	private String hobby;
 
-//	@Column(name = "createdAt", columnDefinition = "datetime")
+	@Column(name = "createdAt", columnDefinition = "datetime")
 	private String createdAt;
 
-//	@Column(name = "updatedAt", columnDefinition = "datetime")
+	@Column(name = "updatedAt", columnDefinition = "datetime")
 	private String updatedAt;
 
-//	@Column(name = "isDeleted", columnDefinition = "bit")
+	@Column(name = "isDeleted", columnDefinition = "bit")
 	private Character isDeleted;
 
 }
