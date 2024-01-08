51d316b83b9c296115592b4b2ac4d881c5a54d65
Cindy <onlyyu147@gmail.com>
2024-01-08 14:27:23 +0800

    modify

--- proj-mingle/src/main/java/com/ispan/mingle/projmingle/domain/WorkBean.java
+++ proj-mingle/src/main/java/com/ispan/mingle/projmingle/domain/WorkBean.java
@@ -1,37 +1,60 @@
 package com.ispan.mingle.projmingle.domain;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
-[workID]
-      ,[fk_lordID]
-      ,[fk_workType]
-      ,[workName]
-      ,[status]
-      ,[notes]
-      ,[fk_district]
-      ,[address]
-      ,[startDate]
-      ,[endDate]
-      ,[numbers]
-      ,[description]
-      ,[workTime]
-      ,[vacation]
-      ,[ageRestriction]
-      ,[genderRestriction]
-      ,[educationRestriction]
-      ,[experienceRestriction]
-      ,[languageRestriction]
-      ,[licenseRestriction]
-      ,[benefits]
-      ,[createdAt]
-      ,[updatedAt]
-      ,[isDeleted]
-      ,[isOnShelf]
-
+@Data
 @Entity
 @Table(name = "Work")
 public class WorkBean {
 
-    private 
+
+    @Id
+    @Column(name = "workID")
+    private Integer workid;
+    [workID]
+
+    @Column(name = "fk_lordID", columnDefinition = "int")
+    private Integer fk_lordid;
+    <fk_lordID, int,>
+
+    @Column(name = "fk_workType", columnDefinition = "nvarchar(10)")
+    private String fk_worktype;
+    <fk_workType, nvarchar(10),>
+
+    @Column(name = "workName", columnDefinition = "nvarchar(30)")
+    private String workname;
+    ,<workName, nvarchar(30),>
+
+    @Column(name = "status", columnDefinition = "varchar(20)")
+    private String status;
+    ,<status, varchar(20),>
+
+
+
+    ,<notes, nvarchar(100),>
+           ,<fk_district, nvarchar(5),>
+           ,<address, nvarchar(200),>
+           ,<startDate, datetime,>
+           ,<endDate, datetime,>
+           ,<numbers, int,>
+           ,<description, nvarchar(500),>
+           ,<workTime, nvarchar(100),>
+           ,<vacation, nvarchar(100),>
+           ,<ageRestriction, nvarchar(100),>
+           ,<genderRestriction, nvarchar(20),>
+           ,<educationRestriction, nvarchar(100),>
+           ,<experienceRestriction, nvarchar(100),>
+           ,<languageRestriction, nvarchar(100),>
+           ,<licenseRestriction, nvarchar(100),>
+           ,<benefits, nvarchar(100),>
+           ,<createdAt, datetime,>
+           ,<updatedAt, datetime,>
+           ,<isDeleted, bit,>
+           ,<isOnShelf, bit,>)
+
+
 }
