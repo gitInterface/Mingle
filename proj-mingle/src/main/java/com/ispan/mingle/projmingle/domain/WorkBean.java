d3cd974fff950618dfe78a72a9ccfec58159591b
Cindy <onlyyu147@gmail.com>
2024-01-08 13:32:37 +0800

    修改bean

--- proj-mingle/src/main/java/com/ispan/mingle/projmingle/domain/WorkBean.java
+++ proj-mingle/src/main/java/com/ispan/mingle/projmingle/domain/WorkBean.java
@@ -1,5 +1,37 @@
 package com.ispan.mingle.projmingle.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+[workID]
+      ,[fk_lordID]
+      ,[fk_workType]
+      ,[workName]
+      ,[status]
+      ,[notes]
+      ,[fk_district]
+      ,[address]
+      ,[startDate]
+      ,[endDate]
+      ,[numbers]
+      ,[description]
+      ,[workTime]
+      ,[vacation]
+      ,[ageRestriction]
+      ,[genderRestriction]
+      ,[educationRestriction]
+      ,[experienceRestriction]
+      ,[languageRestriction]
+      ,[licenseRestriction]
+      ,[benefits]
+      ,[createdAt]
+      ,[updatedAt]
+      ,[isDeleted]
+      ,[isOnShelf]
+
+@Entity
+@Table(name = "Work")
 public class WorkBean {
-    
+
+    private 
 }
