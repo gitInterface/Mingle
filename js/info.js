54da6bce6d76ee3dc21092307e3103d71cad7e97
gitInterface <97436826+gitInterface@users.noreply.github.com>
2024-01-11 12:59:56 +0800

    modify info cancel button

--- js/info.js
+++ js/info.js
@@ -1,7 +1,16 @@
-formFile.onchange = function(event) {
+formFile.onchange = function() {
+    document.getElementById("formImg").style.display = "initial";
     document.getElementById("formImg").style.visibility = "visible";
     const [file] = formFile.files;
     if (file) {
-        formImg.src = URL.createObjectURL(file)
+        formImg.src = URL.createObjectURL(file);
     }
+}
+
+let cancel = document.getElementById("cancel");
+cancel.onclick = function(){
+    document.getElementById("email").value = "";
+    document.getElementById("formFile").value="";
+    document.getElementById("formImg").style.display = "none";
+    document.getElementById("textarea").value = "";
 }
